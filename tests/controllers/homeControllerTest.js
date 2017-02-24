61fdc4128e7e291b26d8e4ab90259a91bfffb307
Joèl Klingler <joel.klingler@hotmail.com>
2017-02-24 09:39:29 +0100

    added test suite

---
+++ tests/controllers/homeControllerTest.js
@@ -0,0 +1,9 @@
+var chai = require('chai');
+var expect = chai.expect;
+var homeController = require('../../app/controllers/homeController');
+
+describe('HomeController', function() {
+    it('The very first test', function() {
+        expect(true).to.equal(true);
+    });
+});
